e9c678857458e966783df57f832474ffaea41ca1
Keshav Dutt Kaushik <keshav.duttkaushik@cognizant.com>
2025-09-02 11:17:46 +0530

    tested code

--- src/main/java/org/codemxde/recorded_lectures/utkarsh_gupta/advanced_dsa_one/assignment/q3_maximum_absolute_difference/Solution.java
+++ src/main/java/org/codemxde/recorded_lectures/utkarsh_gupta/advanced_dsa_one/assignment/q3_maximum_absolute_difference/Solution.java
@@ -1,8 +1,14 @@
 package org.codemxde.recorded_lectures.utkarsh_gupta.advanced_dsa_one.assignment.q3_maximum_absolute_difference;
 
+import java.util.Arrays;
+
 public class Solution {
 
-    public int maxArr(int[] A) {
+    public static void main(String[] args) {
+        System.out.println("Output: " + Arrays.asList(maxArr(new int[] { 1, 3, -1 })));
+    }
+
+    public static int maxArr(int[] A) {
         int maximumX = Integer.MIN_VALUE, minimumX = Integer.MAX_VALUE;
         int maximumY = maximumX, minimumY = minimumX;
 
